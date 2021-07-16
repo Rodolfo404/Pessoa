e2cbfa38268b427460375b4a1bb2dd846c0b51ed
Rodolfo Fernando Silva de Oliveira <84691850+Rodolfo404@users.noreply.github.com>
2021-07-16 16:23:53 -0300

    Add files via upload

---
+++ CadastroPessoa/Model/pessoaFisica.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CadastroPessoa.Endereco
+
+{
+    class PessoaFisica : Pessoa
+    {
+        public string Rg { get; set; }
+        public string Cpf { get; set; }
+
+        public Endereco endereco { get; set; }
+
+
+
+    }
+}
---
+++ CadastroPessoa/Model/pessoaJuridica.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CadastroPessoa.Model
+{
+    class pessoaJuridica : Pessoa
+    {
+        public string Ie { get; set; }
+        public string Cnpj { get; set; }
+        public Endereco endereco { get; set; }
+
+
+
+    }
+}
